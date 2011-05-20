2c1d7bf5f8c01efda2c815fe5c3e713ff8dbde1e
Makoto Ishida <makotoishida@gmail.com>
2011-05-19 20:51:36 -1000

    20110519

--- src/com/example/intent03/HistoryDb.java
+++ src/com/example/intent03/HistoryDb.java
@@ -19,7 +19,6 @@ public class HistoryDb   {
 	public String url = "";
 	public String title = "";
 	
-	
 	public static void init(String pkgName) throws Exception{
 		DbPath = "/data/data/" + pkgName + "/" + DBNAME;
 
@@ -34,6 +33,17 @@ public class HistoryDb   {
 		} finally{
 			db.close();
 		}
+	}
+	
+	//テーブルが無ければ作成する。
+	private static void createTable(SQLiteDatabase db) {
+		String sql = "CREATE TABLE IF NOT EXISTS " + TBL_HISTORY
+					+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, title TEXT);";
+		db.execSQL(sql);
+
+		//urlにインデックスを追加。
+		sql = "CREATE INDEX IF NOT EXISTS 'main'.'ix_history_url' ON 'history' ('url' ASC)";
+		db.execSQL(sql);
 	}
 
 	public static HistoryDb select(String url) throws Exception{
@@ -111,6 +121,22 @@ public class HistoryDb   {
 		}
 	}
 	
+	private static void insertOrUpdateHistory(SQLiteDatabase db, String url, String title) {
+		ContentValues values = new ContentValues();
+		values.put("url", url);
+		values.put("title", title);
+
+		String[] args = { url };
+
+		//同じURLが既に保存されていれば更新する。
+		int n = db.update(TBL_HISTORY, values, "url = ?", args );
+		
+		if (n == 0){
+			//無ければ挿入する。
+			db.insert(TBL_HISTORY, null, values);
+		}
+	}
+
 	public static void delete(String url) throws Exception {
 		SQLiteDatabase db = SQLiteDatabase.openOrCreateDatabase(DbPath, null);
 		try {
@@ -135,30 +161,4 @@ public class HistoryDb   {
 		}
 	}
 
-	private static void insertOrUpdateHistory(SQLiteDatabase db, String url, String title) {
-		ContentValues values = new ContentValues();
-		values.put("url", url);
-		values.put("title", title);
-
-		String[] args = { url };
-
-		//同じURLが既に保存されていれば更新する。
-		int n = db.update(TBL_HISTORY, values, "url = ?", args );
-		
-		if (n == 0){
-			//無ければ挿入する。
-			db.insert(TBL_HISTORY, null, values);
-		}
-	}
-
-	//テーブルが無ければ作成する。
-	private static void createTable(SQLiteDatabase db) {
-		String sql = "CREATE TABLE IF NOT EXISTS " + TBL_HISTORY
-					+ " (_id INTEGER PRIMARY KEY AUTOINCREMENT, url TEXT, title TEXT);";
-		db.execSQL(sql);
-
-		//urlにインデックスを追加。
-		sql = "CREATE INDEX IF NOT EXISTS 'main'.'ix_history_url' ON 'history' ('url' ASC)";
-		db.execSQL(sql);
-	}
 }
