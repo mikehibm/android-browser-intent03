c62ac9557fd899884b0c3da25c0eee804fdac7e2
Makoto Ishida <makotoishida@gmail.com>
2011-05-24 03:41:25 -1000

    Added a library to auto-detect char encoding.

--- src/com/example/intent03/HttpUtil.java
+++ src/com/example/intent03/HttpUtil.java
@@ -1,6 +1,11 @@
 package com.example.intent03;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,6 +17,7 @@ import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.mozilla.universalchardet.UniversalDetector;
 
 import android.util.Log;
 
@@ -31,7 +37,8 @@ public class HttpUtil {
 			HttpEntity entity = httpResponse.getEntity();
 			if (entity != null){
 				byte[] arr = EntityUtils.toByteArray(entity);
-				encoding = findEncoding(entity, arr);
+				encoding = detectEncoding(arr);
+				if (encoding == null) encoding = findEncoding(entity, arr);
 				result = new String(arr, encoding);
 				entity.consumeContent();			// entityのリソースを解放
 			}
@@ -52,37 +59,55 @@ public class HttpUtil {
 		String encoding = "UTF-8";
 		
 		Header enc = entity.getContentEncoding();
-		if (enc != null) encoding = enc.getName();
-		
-		try {
-			String result = new String(arr, encoding);
+		if (enc != null){ 
+			encoding = enc.getName();
+		} else {
+			try {
+				String result = new String(arr, encoding);
 
-			String regexp1 = "<meta (.*)";
-			Pattern pattern = Pattern.compile(regexp1);
-			Matcher matcher = pattern.matcher(result);
-			while (matcher.find()) {
-				String metaline = matcher.group(1);
+				String regexp1 = "<meta (.*)charset(.*)";
+				Pattern pattern = Pattern.compile(regexp1);
+				Matcher matcher = pattern.matcher(result);
+				while (matcher.find()) {
+					String metaline = matcher.group(2);
 
-				//<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />		
-				if (metaline.toLowerCase().contains("charset=shift_jis")){
-					encoding = "Shift_JIS";
-					break;
+					//<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />		
+					if (metaline.toLowerCase().contains("shift_jis")){
+						encoding = "Shift_JIS";
+						break;
+					}
+
+					//<meta http-equiv="Content-Type" content="text/html; charset=euc-jp" />
+					if (metaline.toLowerCase().contains("euc-jp")){
+						encoding = "euc-jp";
+						break;
+					}
 				}
-
-				//<meta http-equiv="Content-Type" content="text/html; charset=euc-jp" />
-				if (metaline.toLowerCase().contains("charset=euc-jp")){
-					encoding = "euc-jp";
-					break;
-				}
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
 			}
-
-			
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
 		}
-		
 		return encoding;
 	} 
+	
+	private static String detectEncoding(byte[] arr){
+       byte[] buf = new byte[4096];
+       ByteArrayInputStream stream = new ByteArrayInputStream(arr);
+		UniversalDetector detector = new UniversalDetector(null);
+
+		try {
+			int nread;
+			while ((nread = stream.read(buf)) > 0 && !detector.isDone()) {
+				detector.handleData(buf, 0, nread);
+			}
+       } catch (IOException e) {
+    	   e.printStackTrace();
+        }
+       detector.dataEnd();
+
+		String encoding = detector.getDetectedCharset();
+		return encoding;
+	}
 	
 	public static String getTitle(String url) {
 		String html = getHtml(url);
@@ -90,10 +115,12 @@ public class HttpUtil {
 		
 		String html_lower = html.toLowerCase();
 		int p = html_lower.indexOf("</head>");
-		html_lower = html_lower.substring(0, p).replace("\n", "").replace("\r", "");
+		if (p > 0) html_lower = html_lower.substring(0, p);
+		
+		html_lower = html_lower.replace("\n", "").replace("\r", "");
 		String title = null;
 
-		String regexp1 = "<title>(.*)</title>";
+		String regexp1 = ".*<title.*>(.*)</title>";
 		Pattern pattern = Pattern.compile(regexp1);
 		Matcher matcher = pattern.matcher(html_lower);
 		while (matcher.find()) {
--- src/com/example/intent03/IntentReceiveActivity.java
+++ src/com/example/intent03/IntentReceiveActivity.java
@@ -30,7 +30,7 @@ public class IntentReceiveActivity extends Activity implements Runnable {
 	
 	private Handler mHandler = new Handler(){
 		public void handleMessage(Message msg){
-			mProgress.dismiss();
+			if (mProgress != null) mProgress.dismiss();
 
 			Bundle bundle = msg.getData();
 			String url = bundle.getString("url");
@@ -53,7 +53,6 @@ public class IntentReceiveActivity extends Activity implements Runnable {
         setContentView(R.layout.main);
         
         if (adapter == null){
-        	//adapter = new ArrayAdapter<HistoryDb.HistoryItem>(this, android.R.layout.simple_list_item_1);
         	ArrayList<HistoryDb.HistoryItem> arraylist = new ArrayList<HistoryDb.HistoryItem>();
         	adapter = new ListItemAdapter(this, 0, arraylist);
         }
@@ -126,9 +125,23 @@ public class IntentReceiveActivity extends Activity implements Runnable {
     	if (Intent.ACTION_VIEW.equals(intent.getAction()) ){
 			try {
 				if (isConnected()){
+					Log.d("inten04", "describeContents=" + intent.describeContents());
+					Log.d("inten04", "Type=" + intent.getType());
+					if (intent.getCategories() != null){
+						Log.d("inten04", "Categories().size=" + intent.getCategories().size());
+					} else {
+						Log.d("inten04", "Categories()=null");
+					}
+					Log.d("inten04", "toUri(0)=" + intent.toUri(0));
+					Log.d("inten04", "Flags=" + intent.getFlags());
+					
 					//標準ブラウザで開く
 					intent.setClassName("com.android.browser", "com.android.browser.BrowserActivity");
 					startActivity(intent);
+
+					//データベースに保存。
+					String url = intent.getDataString();
+					HistoryDb.save(url, null);
 
 					//別スレッドでタイトルを取得
 					startGettingTitle();
